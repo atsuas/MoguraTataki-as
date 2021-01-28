151d202bce3066b2d71e9664506318593b51d692
as <>
2021-01-29 04:47:30 +0900

    モグラを叩けるようにする

--- Assets/MyScript/MoleController.cs
+++ Assets/MyScript/MoleController.cs
@@ -4,15 +4,56 @@ using UnityEngine;
 
 public class MoleController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    Vector3 groundLevel;
+    Vector3 undergroundLevel;
+    public GameObject effect;
+
+    bool isOnGround = true;
+    float time = 0;
+
+
+    void Up()
     {
-        
+        transform.position = groundLevel;
+        this.isOnGround = true;
     }
 
-    // Update is called once per frame
+    void Down()
+    {
+        transform.position = this.undergroundLevel;
+        this.isOnGround = false;
+    }
+
+    void Start()
+    {
+        this.groundLevel = transform.position;
+        this.undergroundLevel = this.groundLevel - new Vector3(0, 0.2f, 0);
+
+        Down();
+    }
+
     void Update()
     {
-        
+        this.time += Time.deltaTime;
+
+        if (this.time > 2.0f)
+        {
+            this.time = 0;
+            if (this.isOnGround)
+            {
+                Down();
+            }
+            else
+            {
+                Up();
+            }
+        }
     }
+
+    public void Hit()
+    {
+        this.time = 0;
+        Down();
+    }
+
 }
